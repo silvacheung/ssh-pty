40d25d9891d28a2c989ad1acf801570e13a892bf
zhangxiaojun <1214934780@qq.com>
2024-07-15 20:02:15 +0800

    配置优化

--- yaml/kubernetes/v1.30/control-plane/set-cni-cilium.sh
+++ yaml/kubernetes/v1.30/control-plane/set-cni-cilium.sh
@@ -18,9 +18,11 @@ helm upgrade --install cilium cilium/cilium \
   --set operator.replicas=2 \
   --set ipam.mode=kubernetes \
   --set k8s.requireIPv4PodCIDR=true \
-  --set kubeProxyReplacement=true \
+  --set k8sClientRateLimit.qps=15 \
+  --set k8sClientRateLimit.burst=30 \
   --set k8sServiceHost={{ get "config.k8s.control_plane_endpoint.address" }} \
   --set k8sServicePort={{ get "config.k8s.control_plane_endpoint.port" }} \
+  --set kubeProxyReplacement=true \
   --set kubeProxyReplacementHealthzBindAddr=0.0.0.0:10256 \
   --set containerRuntime.integration=containerd \
   --set bandwidthManager.enabled=true \
@@ -28,11 +30,13 @@ helm upgrade --install cilium cilium/cilium \
   --set localRedirectPolicy=true \
   --set internalTrafficPolicy=Cluster \
   --set externalTrafficPolicy=Cluster \
-  --set loadBalancer.acceleration=best-effort \
+  --set loadBalancer.acceleration=disabled \
   --set loadBalancer.serviceTopology=true \
   --set loadBalancer.algorithm=maglev \
   --set maglev.tableSize=131071 \
   --set maglev.hashSeed=$(head -c12 /dev/urandom | base64 -w0) \
+  --set socketLB.enabled=true \
+  --set socketLB.hostNamespaceOnly=true \
   --set bpf.masquerade=true \
   --set bpf.lbExternalClusterIP=true \
   --set bpf.mapDynamicSizeRatio=0.0025 \
@@ -43,6 +47,10 @@ helm upgrade --install cilium cilium/cilium \
   --set bpf.natMax=786432 \
   --set bpf.neighMax=786432 \
   --set bpf.policyMapMax=32768 \
+  --set bpf.tproxy=true \
+  --set bpfClockProbe=true \
+  --set hostFirewall.enabled=true \
+  --set l2announcements.enabled=true \
   --set encryption.enabled=false \
   --set encryption.nodeEncryption=false \
   --set encryption.type=wireguard \
@@ -63,11 +71,18 @@ helm upgrade --install cilium cilium/cilium \
 #  --set ipv4NativeRoutingCIDR=10.0.0.0/8 \
 #  --set autoDirectNodeRoutes=true \
 #  --set installNoConntrackIptablesRules=true \
-#  --set socketLB.hostNamespaceOnly=true \
 #  --set enableIPv4BIGTCP=true \
+
+# cilium metrics (hubble.enabled must to true)
+#  --set prometheus.enabled=false \
+#  --set operator.prometheus.enabled=false \
+#  --set hubble.metrics.enableOpenMetrics=false \
+#  --set hubble.metrics.enabled="{dns,drop,tcp,flow,port-distribution,icmp,httpV2:exemplars=true;labelsContext=source_ip\,source_namespace\,source_workload\,destination_ip\,destination_namespace\,destination_workload\,traffic_direction}"
 
 #查看cilium状态
 #kubectl -n kube-system exec ds/cilium -- cilium-dbg status --verbose
+#kubectl -n kube-system rollout restart deployment/cilium-operator
+#kubectl -n kube-system rollout restart ds/cilium
 
 #echo "安装Cilium >> 滚动更新Coredns"
 #kubectl rollout restart deployment/coredns -n kube-system
